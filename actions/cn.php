de60f0c815b9a6df0a3d9e860e6d95a8453da20d
Asad Mukhtar <axad03213@gmail.com>
2026-09-05 18:47:23 +0500

    all code

---
+++ actions/cn.php
@@ -0,0 +1,4 @@
+<?php
+         // connection ...
+        $cn = mysqli_connect("localhost","root","","trendmart") or die('cant connect with database..!'); 
+?>
--- actions/signup.php
+++ actions/signup.php
@@ -4,7 +4,13 @@
     $password = $_POST['password'];
     $cpassword = $_POST['confirm_password'];    
     if($password == $cpassword){
-        $cn = mysqli_connect("localhost","root","","trendmart") or die('cant connect with database..!');
+       include('cn.php'); // connection ..
+       /*
+        - Logic 
+        - Step 1: Proccess check kry ga k is email k against account already ha ya ni 
+        - Step 2: Agr ni ha to bna do wrna error dy do that email already exist 
+       */
+        // Query ...
         $query = "INSERT INTO `users`(name,email,password) VALUES ('$name','$email','$password')";
       //  var_dump($query);
         mysqli_query($cn,$query) or die('cant run query');
