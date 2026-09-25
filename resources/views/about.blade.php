c11b5669a83609924128fdbc6c955ddcbad4098a
Asad Mukhtar <axad03213@gmail.com>
2026-09-25 18:46:34 +0500

    authenticaiton system in laravel

---
+++ resources/views/about.blade.php
@@ -0,0 +1,1 @@
+<h2>I am About page</h2>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/about',function(){
+    return view('about');
+});
+Route::get('/contact',function(){
+    return view('contact');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
