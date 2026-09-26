4dde226ded7d70d70c0d9b83786123dd25a2c886
Asad Mukhtar <axad03213@gmail.com>
2026-09-26 18:50:34 +0500

    creating views

--- app/Http/Controllers/pagesController.php
+++ app/Http/Controllers/pagesController.php
@@ -18,4 +18,27 @@ class pagesController extends Controller
     public function contact(){
         return view('contact');
     }
+    // products..
+    public function products(){
+        return view('products');
+    }
+    // Show Product..
+    public function showproduct(){
+        return view('showproduct');
+    }
+    // faq 
+    public function faq(){
+        return view('faq');
+    }
+    // checkout ..
+    public function checkout(){
+        return view('checkout');
+    }
+    // privacy polcies ..
+    public function privacy(){
+        return view('privacy');
+    }
+    public function cart(){
+        return view('cart');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,13 @@ use App\Http\Controllers\pagesController;
 Route::get('/', [pagesController::class,'home'])->name('home');
 Route::get('/about',[pagesController::class,'about'])->name('about');
 Route::get('/contact',[pagesController::class,'contact'])->name('contact');
+Route::get('/products',[pagesController::class,'products'])->name('products');
+Route::get('/showproduct',[pagesController::class,'showproduct'])->name('showproduct');
+Route::get('/faq',[pagesController::class,'faq'])->name('faq');
+Route::get('/checkout',[pagesController::class,'checkout'])->name('checkout');
+Route::get('/privacy',[pagesController::class,'privacy'])->name('privacy');
+Route::get('/cart',[pagesController::class,'cart'])->name('cart');
+
 
 Auth::routes();
 
